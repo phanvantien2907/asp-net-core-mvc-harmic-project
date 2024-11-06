58e441e95e8558528dd3657b20332db4fb51522d
phanvantien2907 <vantienp530@gmail.com>
2024-11-06 21:50:54 +0700

    update code

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67aaba5821d568dca3406e48a1cac8dbcd68e511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f6bbf824b74797f00b20511b72331405789a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
